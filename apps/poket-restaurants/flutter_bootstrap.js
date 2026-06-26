b4a336916fc4689cc900ee9ea375f53a3d294416
boulamobil-svg <boula.mobil@gmail.com>
2026-06-26 09:43:00 +0200

    Publish Poket Restaurants 1.0.6+17

--- apps/poket-restaurants/flutter_bootstrap.js
+++ apps/poket-restaurants/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1932429097" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "372394852" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
