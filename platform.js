ae1afd040602702e6250066297905ef87b309fbf
boulamobil-svg <boula.mobil@gmail.com>
2026-07-21 18:40:28 +0200

    Publish Poket Restaurants Android 1.0.6+29

--- platform.js
+++ platform.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
 
 const DOWNLOADS = {
   web: "/apps/poket-restaurants/",
-  android: "https://poksol.com/downloads/poket-restaurants/android/chez_marwan_pos_1.0.6+28.apk",
+  android: "https://poksol.com/downloads/poket-restaurants/android/chez_marwan_pos_1.0.6+29.apk",
   windows: "https://poksol.com/downloads/poket-restaurants/windows/poket_restaurants_windows_1.0.6+28.zip"
 };
 
